f368ae2e38a2469987679003ac462a272b6267e8
Steve Gray <heystevegray.dev@gmail.com>
2026-03-31 13:41:19 -0400

    .

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,14 +1,14 @@
-export const MY_WEBSITE_URL = 'https://www.stevegray.io/'
-export const MY_NAME = 'Steve Gray'
+export const MY_WEBSITE_URL = "https://stevegray.io/";
+export const MY_NAME = "Steve Gray";
 
-export type SiteConfig = typeof siteConfig
+export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: 'veelee',
-  description: 'Get framer video links.',
-  mainNav: [],
-  links: {
-    github: 'https://github.com/heystevegray/veelee',
-  },
-  appURL: 'https://veelee.vercel.app/',
-}
+	name: "veelee",
+	description: "Get framer video links.",
+	mainNav: [],
+	links: {
+		github: "https://github.com/heystevegray/veelee",
+	},
+	appURL: "https://veelee.vercel.app/",
+};
